434be683fb611ca47a089c8d0a05f7b8b24a4a96
kshim-personal-notebook-window <a41533503@gmail.com>
2022-11-14 18:02:35 +0900

    v2

--- include/philosophers.h
+++ include/philosophers.h
@@ -6,7 +6,25 @@
 /*   By: kshim <kshim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 16:56:00 by kshim             #+#    #+#             */
-/*   Updated: 2022/11/14 16:56:01 by kshim            ###   ########.fr       */
+/*   Updated: 2022/11/14 18:02:21 by kshim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+typedef struct s_program_data{
+	int philo_num;
+	int time_to_eat;
+	int time_to_sleep;
+	int number_to_eat;
+	t_mntor	*monitor;
+	t_philo	*philo_array;
+}			t_prg;
+
+
+typedef struct s_monitor{
+	/* data */
+}			t_mntor;
+
+typedef struct s_philosopher{
+
+}			t_philo;
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,27 @@
 /*   By: kshim <kshim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 16:54:31 by kshim             #+#    #+#             */
-/*   Updated: 2022/11/14 16:54:35 by kshim            ###   ########.fr       */
+/*   Updated: 2022/11/14 17:18:59 by kshim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// error 함수 만들고, 입력값으로 텍스트, 에러 코드 받아서 메인에서 반환하게 만들기?
+
+int main(int argc, char *argv)
+{
+	t_prg	prg;
+
+	if (argc != 5 || argc != 6)
+		return (1);
+	// philosophers 구조체 시작 및 초기화
+		// philosophers 메인 데이터 구조체
+		// philosophers 철학자 구조체
+		// philosophers 감시 구조체
+	if (ft_init_prg(argc, argv, &prg) != 1)
+		return (1);
+	// philosophers 철학자 스레드 실행
+	// 검사하다가 문제 생기면 - 철학자 사망 - 탈출하여 main까지 반환 (exit 사용 불가? 였던 것 같음)
+	if (ft_phiosophers_start(prg) != 1)
+		return (1);
+	return (0);
+}
